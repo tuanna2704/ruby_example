5d550b2bbcf69d07985fa648ef969a31eeb3f412
Anh Tuan <tuanna2704@gmail.com>
2021-08-05 17:00:22 +0700

    fix angular error decorator

--- babel.config.js
+++ babel.config.js
@@ -49,6 +49,7 @@ module.exports = function(api) {
     plugins: [
       'babel-plugin-macros',
       '@babel/plugin-syntax-dynamic-import',
+      ["@babel/plugin-proposal-decorators", { "legacy": true }],
       isTestEnv && 'babel-plugin-dynamic-import-node',
       '@babel/plugin-transform-destructuring',
       [
